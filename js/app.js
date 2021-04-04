42d88b727f8ef410169070edf0950ebc947738f6
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-04-04 16:18:14 +0300

    some changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 let userName = prompt('please enter your name!')
 let occupation = prompt('what\'s your occupation ?')
+let Q = prompt('Are you a mechanical designer or not ?')
 if (userName!= ''){
     alert ('your name is : ' + userName+ ' ,and your occupation is : ' + occupation);
+
 }else{
     alert ('welcome to my website');
-}
+} 
+   console.log(userName)
+    console.log(occupation)
+console.log(Q);
