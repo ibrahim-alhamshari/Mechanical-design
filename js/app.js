3b076c732a0f9b46b67735d37a2aedf83b9a7dbb
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-04-04 16:06:19 +0300

    Mechanical design

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let userName = prompt('please enter your name!')
+let occupation = prompt('what\'s your occupation ?')
+if (userName!= ''){
+    alert ('your name is : ' + userName+ ' ,and your occupation is : ' + occupation);
+}else{
+    alert ('welcome to my website');
+}
